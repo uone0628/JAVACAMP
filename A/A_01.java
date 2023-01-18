e56e0cbba6dd97c90c63b4a23f6c230730f46dca
uone0628 <121482693+uone0628@users.noreply.github.com>
2023-01-19 03:12:27 +0900

    Add Step A

--- A/A_01.java
+++ A/A_01.java
@@ -5,14 +5,14 @@ class Main {
     Main stepA = new Main();
     stepA.a01();
   }
-  
-  void a01(){
+
+  void a01() {
     Scanner s = new Scanner(System.in);
-  	System.out.print("Enter a birth year > ");
-  	int birthyear = s.nextInt();
-  	int age = 2022 - birthyear + 1;
-    
-  	System.out.println("You are "+age+" years old.");
+    System.out.print("Enter a birth year > ");
+    int birthyear = s.nextInt();
+    int age = 2022 - birthyear + 1;
+
+    System.out.println("You are " + age + " years old.");
 
   }
 }
